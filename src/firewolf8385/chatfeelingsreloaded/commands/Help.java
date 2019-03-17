a57df8ae4514eedf965aa8f7f930a6206507d163
Colby <colbykrawczyk10@gmail.com>
2019-03-17 17:09:04 -0400

    Fixed "cfr.use.actions and cfr.use.feelings" do not work.

--- src/firewolf8385/chatfeelingsreloaded/commands/Help.java
+++ src/firewolf8385/chatfeelingsreloaded/commands/Help.java
@@ -23,138 +23,150 @@ public class Help implements CommandExecutor
 
         if(label.equalsIgnoreCase("actions"))
         {
-            if(args.length != 0)
+            if(sender.hasPermission("cfr.use.actions"))
             {
-                switch(args[0])
+                if(args.length != 0)
                 {
-                    case "2":
-                        utils.sendChat(p, grey + "----------" + gold1 + "Actions " + yellow + "[2/4]" + grey + "----------");
-                        utils.sendChat(p, settings.getConfig().getString("Highfive-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Hug-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Kiss-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Lick-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Love-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Murder-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Pat-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Poke-Help"));
-                        utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 3 " + lightGrey + "for the next page.");
-                        break;
+                    switch(args[0])
+                    {
+                        case "2":
+                            utils.sendChat(p, grey + "----------" + gold1 + "Actions " + yellow + "[2/4]" + grey + "----------");
+                            utils.sendChat(p, settings.getConfig().getString("Highfive-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Hug-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Kiss-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Lick-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Love-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Murder-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Pat-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Poke-Help"));
+                            utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 3 " + lightGrey + "for the next page.");
+                            break;
 
-                    case "3":
-                        utils.sendChat(p, grey + "----------" + gold1 + "Actions " + yellow + "[3/4]" + grey + "----------");
-                        utils.sendChat(p, settings.getConfig().getString("Punch-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Scorn-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Shake-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Shoes-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Sing-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Slap-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Sleep-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Snuggle-Help"));
-                        utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 3 " + lightGrey + "for the next page.");
-                        break;
+                        case "3":
+                            utils.sendChat(p, grey + "----------" + gold1 + "Actions " + yellow + "[3/4]" + grey + "----------");
+                            utils.sendChat(p, settings.getConfig().getString("Punch-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Scorn-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Shake-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Shoes-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Sing-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Slap-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Sleep-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Snuggle-Help"));
+                            utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 3 " + lightGrey + "for the next page.");
+                            break;
 
-                    case "4":
-                        utils.sendChat(p, grey + "----------" + gold1 + "Actions " + yellow + "[4/4]" + grey + "----------");
-                        utils.sendChat(p, settings.getConfig().getString("Spell-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Stab-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Stalk-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Wave-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Whisper-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Wink-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Yell-Help"));
-                        utils.sendChat(p, "");
-                        utils.sendChat(p, "");
-                        break;
+                        case "4":
+                            utils.sendChat(p, grey + "----------" + gold1 + "Actions " + yellow + "[4/4]" + grey + "----------");
+                            utils.sendChat(p, settings.getConfig().getString("Spell-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Stab-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Stalk-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Wave-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Whisper-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Wink-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Yell-Help"));
+                            utils.sendChat(p, "");
+                            utils.sendChat(p, "");
+                            break;
 
-                    default:
-                        utils.sendChat(p,grey + "----------" + gold1 + "Actions " + yellow + "[1/4]" + grey + "----------");
-                        utils.sendChat(p, settings.getConfig().getString("Bite-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Blush-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Boi-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Cringe-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Cry-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Dab-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Fight-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Hide-Help"));
-                        utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 2 " + lightGrey + "for the next page.");
-                        break;
+                        default:
+                            utils.sendChat(p,grey + "----------" + gold1 + "Actions " + yellow + "[1/4]" + grey + "----------");
+                            utils.sendChat(p, settings.getConfig().getString("Bite-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Blush-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Boi-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Cringe-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Cry-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Dab-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Fight-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Hide-Help"));
+                            utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 2 " + lightGrey + "for the next page.");
+                            break;
+                        }
+                    }
+                else
+                {
+                    utils.sendChat(p,grey + "----------" + gold1 + "Actions " + yellow + "[1/4]" + grey + "----------");
+                    utils.sendChat(p, settings.getConfig().getString("Bite-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Blush-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Boi-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Cringe-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Cry-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Dab-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Fight-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Hide-Help"));
+                    utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 2 " + lightGrey + "for the next page.");
                 }
             }
             else
             {
-                utils.sendChat(p,grey + "----------" + gold1 + "Actions " + yellow + "[1/4]" + grey + "----------");
-                utils.sendChat(p, settings.getConfig().getString("Bite-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Blush-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Boi-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Cringe-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Cry-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Dab-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Fight-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Hide-Help"));
-                utils.sendChat(p, lightGrey + "Do " + gold2 + "/actions 2 " + lightGrey + "for the next page.");
+                utils.sendChat(p, settings.getConfig().getString("NoPermissions"));
             }
-
         }
         else if(label.equalsIgnoreCase("feelings"))
         {
-            if(args.length != 0)
+            if(p.hasPermission("cfr.use.feelings"))
             {
-                switch(args[0])
+                if(args.length != 0)
                 {
-                    case "2":
-                        utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[2/3]" + grey + "----------");
-                        utils.sendChat(p, settings.getConfig().getString("Guilty-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Happy-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Jealous-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Jolly-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Lonely-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Loved-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Nervous-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Sad-Help"));
-                        utils.sendChat(p, lightGrey + "Do " + gold2 + "/feelings 3 " + lightGrey + "for the next page.");
-                        break;
+                    switch(args[0])
+                    {
+                        case "2":
+                            utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[2/3]" + grey + "----------");
+                            utils.sendChat(p, settings.getConfig().getString("Guilty-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Happy-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Jealous-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Jolly-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Lonely-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Loved-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Nervous-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Sad-Help"));
+                            utils.sendChat(p, lightGrey + "Do " + gold2 + "/feelings 3 " + lightGrey + "for the next page.");
+                            break;
 
-                    case "3":
-                        utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[3/3]" + grey + "----------");
-                        utils.sendChat(p, settings.getConfig().getString("Scared-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Shy-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Worried-Help"));
-                        utils.sendChat(p, "");
-                        utils.sendChat(p, "");
-                        utils.sendChat(p, "");
-                        utils.sendChat(p, "");
-                        utils.sendChat(p, "");
-                        utils.sendChat(p, "");
-                        break;
+                        case "3":
+                            utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[3/3]" + grey + "----------");
+                            utils.sendChat(p, settings.getConfig().getString("Scared-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Shy-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Worried-Help"));
+                            utils.sendChat(p, "");
+                            utils.sendChat(p, "");
+                            utils.sendChat(p, "");
+                            utils.sendChat(p, "");
+                            utils.sendChat(p, "");
+                            utils.sendChat(p, "");
+                            break;
 
-                    default:
-                        utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[1/3]" + grey + "----------");
-                        utils.sendChat(p, settings.getConfig().getString("Afraid-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Angry-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Annoyed-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Bored-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Calm-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Confused-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Crazy-Help"));
-                        utils.sendChat(p, settings.getConfig().getString("Excited-Help"));
-                        utils.sendChat(p, lightGrey + "Do " + gold2 + "/feelings 2 " + lightGrey + "for the next page.");
-                        break;
+                        default:
+                            utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[1/3]" + grey + "----------");
+                            utils.sendChat(p, settings.getConfig().getString("Afraid-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Angry-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Annoyed-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Bored-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Calm-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Confused-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Crazy-Help"));
+                            utils.sendChat(p, settings.getConfig().getString("Excited-Help"));
+                            utils.sendChat(p, lightGrey + "Do " + gold2 + "/feelings 2 " + lightGrey + "for the next page.");
+                            break;
+                    }
+                }
+                else
+                {
+                    utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[1/3]" + grey + "----------");
+                    utils.sendChat(p, settings.getConfig().getString("Afraid-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Angry-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Annoyed-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Bored-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Calm-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Confused-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Crazy-Help"));
+                    utils.sendChat(p, settings.getConfig().getString("Excited-Help"));
+                    utils.sendChat(p, lightGrey + "Do " + gold2 + "/feelings 2 " + lightGrey + "for the next page.");
                 }
             }
             else
             {
-                utils.sendChat(p, grey + "----------" + gold1 + "Feelings " + yellow + "[1/3]" + grey + "----------");
-                utils.sendChat(p, settings.getConfig().getString("Afraid-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Angry-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Annoyed-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Bored-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Calm-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Confused-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Crazy-Help"));
-                utils.sendChat(p, settings.getConfig().getString("Excited-Help"));
-                utils.sendChat(p, lightGrey + "Do " + gold2 + "/feelings 2 " + lightGrey + "for the next page.");
+                utils.sendChat(p, settings.getConfig().getString("NoPermission"));
             }
-
         }
 
         return true;
