777277b1d2061583f3563fe74a5f78acb23143c5
Colby <colbykrawczyk10@gmail.com>
2020-07-08 05:17:12 -0400

    Added method to get server version.

--- src/main/java/org.firewolf8385.chatfeelingsreloaded/ChatFeelingsReloaded.java
+++ src/main/java/org.firewolf8385.chatfeelingsreloaded/ChatFeelingsReloaded.java
@@ -1,14 +1,15 @@
 package org.firewolf8385.chatfeelingsreloaded;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChatFeelingsReloaded extends JavaPlugin
 {
     Settings settings = Settings.getInstance();
+    private static int subVersion;
 
     @Override
-    public void onEnable()
-    {
+    public void onEnable() {
         // Setup settings.
         settings.setup(this);
 
@@ -17,38 +18,42 @@ public class ChatFeelingsReloaded extends JavaPlugin
         registerCommands();
         registerFeelings();
         registerListeners();
+
+        // Get server version.
+        String version = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+        subVersion = Integer.parseInt(version.replace("1_", "").replaceAll("_R\\d", "").replace("v", ""));
     }
 
     /**
      * Register the actions made in the config.
      */
-    private void registerActions()
-    {
+    private void registerActions() {
 
     }
 
     /**
      * Register commands used by the plugin.
      */
-    private  void registerCommands()
-    {
+    private  void registerCommands() {
 
     }
 
     /**
      * Register feelings made in the config.
      */
-    private void registerFeelings()
-    {
+    private void registerFeelings() {
 
     }
 
     /**
      * Register listeners used by the plugin.
      */
-    private void registerListeners()
-    {
+    private void registerListeners() {
 
+    }
+
+    public static int getServerVersion() {
+        return subVersion;
     }
 
 }
